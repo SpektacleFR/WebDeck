f1fbbfda57571e839abc0f0c7a9f7d434917f063
Zachary Gough <zacharygough07@gmail.com>
2022-12-30 13:38:09 -0500

    First Commit

---
+++ custom/PyWin.py
@@ -0,0 +1,9 @@
+import win32api
+import win32gui
+
+def mute():
+    WM_APPCOMMAND = 0x319
+    APPCOMMAND_MICROPHONE_VOLUME_MUTE = 0x180000
+
+    hwnd_active = win32gui.GetForegroundWindow()
+    win32api.SendMessage(hwnd_active, WM_APPCOMMAND, None, APPCOMMAND_MICROPHONE_VOLUME_MUTE)
---
+++ custom/powershell.py
@@ -0,0 +1,5 @@
+import subprocess
+
+def run(cmd):
+    completed = subprocess.run(["powershell", "-Command", cmd], capture_output=True)
+    return completed
